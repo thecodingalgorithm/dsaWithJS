c0863b7a7fe5543f5d0def2f20794b27f1d0f80b
thecodingalgorithm <thecodingalgorithm@gmail.com>
2023-03-04 09:51:35 +0100

    codeVolution folder added"

---
+++ codeVolution/index.ts
@@ -0,0 +1,12 @@
+function fibonacciTs(n: number): object {
+    const fib: object = [0, 1]
+
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+
+    return fib
+}
+
+
+console.log(fibonacciTs(5))
